32ce41e846b52a72f605808eb7a563d2b9652ea3
Emil Shalaumov <emil.shalaumov@gmail.com>
2019-11-24 15:17:04 +0300

    Commit ImageModel file

---
+++ CoreDataHomeWork/ImageModel.swift
@@ -0,0 +1,19 @@
+//
+//  ImageModel.swift
+//  CoreDataHomeWork
+//
+//  Created by Эмиль Шалаумов on 24.11.2019.
+//  Copyright © 2019 Emil Shalaumov. All rights reserved.
+//
+
+import UIKit
+
+struct ImageModel {
+    let path: String
+    let description: String
+}
+
+struct ImageViewModel {
+    let description: String
+    let image: UIImage
+}
