24465c6dad9b8378a4bfd6dd9538e9550160923c
Emil Shalaumov <emil.shalaumov@gmail.com>
2019-11-24 12:49:25 +0300

    Initial Commit

---
+++ CoreDataHomeWork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataHomeWork
+//
+//  Created by Эмиль Шалаумов on 24.11.2019.
+//  Copyright © 2019 Emil Shalaumov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
